742bec27dd66e81f309722763573100ed5e0e946
ashtyn3 <ashtyn@gitmono.com>
2021-06-25 09:16:05 -0400

    fixed wallet module

--- wallet/store.go
+++ wallet/store.go
@@ -6,7 +6,6 @@ import (
 	"crypto/sha256"
 	"encoding/gob"
 	"errors"
-	"fmt"
 	"log"
 	"os"
 
@@ -18,7 +17,9 @@ type Options struct {
 	Password string
 }
 
-func InitWallet(options *Options, pairs []KeyPair) error {
+type Wallet map[string]*KeyPair
+
+func (w *Wallet) InitWallet(name string, options *Options) error {
 	if options.Password == "" {
 		return errors.New("Password not provided")
 	}
@@ -29,16 +30,64 @@ func InitWallet(options *Options, pairs []KeyPair) error {
 
 	var content bytes.Buffer
 
-	gob.Register(elliptic.P256())
+	//gob.Register(elliptic.P256())
+	gob.Register(&elliptic.CurveParams{})
 
 	enc := gob.NewEncoder(&content)
-	enc.Encode(pairs)
+	err := enc.Encode(w)
+
+	if err != nil {
+		log.Fatalln("Encode: ", err)
+	}
 	sha := sha256.Sum256([]byte(options.Password))
 	e, err := crypto.Encrypt(sha[:], content.Bytes())
 
 	if err != nil {
 		log.Fatalln(err)
 	}
-	os.WriteFile(options.Path+fmt.Sprintf("%x", pairs[0].Public)[0:11], e, 0644)
+	os.WriteFile(options.Path+name+".OOX_WALLET", e, 0644)
 	return nil
+}
+
+func Add(name, pwd string, pair *KeyPair) {
+	m := GetPairs(name, pwd)
+
+	addr := pair.Addr()
+	m[addr] = pair
+
+	m.InitWallet(name, &Options{Password: pwd})
+}
+
+func Get(name, pwd string, addr string) (*KeyPair, error) {
+	m := GetPairs(name, pwd)
+
+	if m[addr] == nil {
+		return nil, errors.New(addr + " does not exist in wallet")
+	}
+
+	return m[addr], nil
+}
+
+func GetPairs(name, pwd string) Wallet {
+	sha := sha256.Sum256([]byte(pwd))
+	EContent, _ := os.ReadFile(name + ".OOX_WALLET")
+	DContent, err := crypto.Decrypt(sha[:], EContent)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var kpArray Wallet
+
+	//gob.Register(elliptic.P256())
+	gob.Register(&elliptic.CurveParams{})
+
+	dec := gob.NewDecoder(bytes.NewReader(DContent))
+	err = dec.Decode(&kpArray)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return kpArray
 }

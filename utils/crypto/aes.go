6f4ed44fdf10f64c567481f1efa592319ecb1ab6
ashtyn3 <ashtyn@gitmono.com>
2021-06-25 09:18:53 -0400

    added aes wrapper to crypto module

--- utils/crypto/aes.go
+++ utils/crypto/aes.go
@@ -26,3 +26,24 @@ func Encrypt(key, data []byte) ([]byte, error) {
 
 	return ciphertext, nil
 }
+
+func Decrypt(key, data []byte) ([]byte, error) {
+	blockCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(blockCipher)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce, ciphertext := data[:gcm.NonceSize()], data[gcm.NonceSize():]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
